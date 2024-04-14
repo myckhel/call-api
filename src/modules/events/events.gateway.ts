cc36b8ea9310b6e41c7cc04192bedeb3e356d7df
Myckhel <myckhel123@gmail.com>
2024-04-14 15:35:45 +0100

    implemented peer calling

--- src/modules/events/events.gateway.ts
+++ src/modules/events/events.gateway.ts
@@ -5,6 +5,7 @@ import {
   OnGatewayConnection,
   OnGatewayInit,
   WebSocketServer,
+  MessageBody,
 } from '@nestjs/websockets';
 import { EventsService } from './events.service';
 import { Server, Socket } from 'socket.io';
@@ -80,5 +81,99 @@ export class EventsGateway
     );
 
     return memberSockets.map(({ data }) => data?.user);
+  }
+
+  async findUserSockets(_id: string | number) {
+    const socketIds = [];
+
+    (await this.server.sockets.fetchSockets()).map(({ data, id }) => {
+      if (data.user.id === _id) {
+        socketIds.push(id);
+      }
+    });
+
+    return socketIds;
+  }
+
+  @SubscribeMessage('call-data')
+  async callData() {
+    try {
+      // const res = await this.twilioClient.tokens.create();
+      return {
+        iceServers: [{ urls: 'stun:stun.l.google.com:19302' }],
+      };
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  @SubscribeMessage('outgoing-call')
+  async callUser(@MessageBody() data: any) {
+    try {
+      const sockets = await this.findUserSockets(data.to);
+
+      if (sockets?.length) {
+        this.server.to(sockets).emit('incoming-call', data);
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  @SubscribeMessage('answer-incoming-call')
+  async answerCall(@MessageBody() data: any) {
+    try {
+      const sockets = await this.findUserSockets(data.to);
+
+      if (sockets?.length) {
+        this.server.to(sockets).emit('outgoing-answered', {
+          from: data.from,
+          answer: data.answer,
+        });
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  @SubscribeMessage('reject-incoming')
+  async rejectCall(@MessageBody() data: any) {
+    try {
+      const sockets = await this.findUserSockets(data.to);
+
+      if (sockets?.length) {
+        this.server.to(sockets).emit('outgoing-rejected', {
+          from: data.from,
+        });
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  @SubscribeMessage('candidate')
+  async candidate(@MessageBody() data: any) {
+    try {
+      const sockets = await this.findUserSockets(data.to);
+
+      if (sockets?.length) {
+        this.server.to(sockets).emit('candidate', data);
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  @SubscribeMessage('end-call')
+  async endCall(@MessageBody() data: any) {
+    try {
+      const sockets = await this.findUserSockets(data.to);
+
+      if (sockets?.length) {
+        this.server.to(sockets).emit('call-ended', data);
+      }
+    } catch (error) {
+      console.error(error);
+    }
   }
 }
